1d3b629999a0c0bf72ea445d7c71d5d9873e19fd
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-07-29 16:33:20 +0200

    Keep iwyu sources and scripts
      * Also symlink python to python3

--- iwyu.sh
+++ iwyu.sh
@@ -5,12 +5,8 @@ pushd /opt
 git clone https://github.com/include-what-you-use/include-what-you-use.git
 pushd include-what-you-use
 git checkout clang_12
-popd
 
-mkdir iwyu
-pushd iwyu
-cmake -G "Unix Makefiles" -DCMAKE_PREFIX_PATH=/usr/lib/llvm-12 ../include-what-you-use
+mkdir build
+pushd build
+cmake -DCMAKE_PREFIX_PATH=/usr/lib/llvm-12 ..
 cmake --build .
-popd
-
-rm -rf include-what-you-use
